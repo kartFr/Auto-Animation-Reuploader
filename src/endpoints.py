bf78b1ba25e869dcf6cb5fee8ce52af6ee285b17
Kuromi Care <155983154+kuromian@users.noreply.github.com>
2024-08-27 17:58:08 -0500

    Update endpoints.py
    user_info updated

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,14 +1,14 @@
 github_repo_latest = "https://api.github.com/repos/kartFr/Auto-Animation-Reuploader/releases/latest"
 asset_delivery = "https://assetdelivery.roblox.com/v1/asset/?id="
 asset_info = "https://develop.roblox.com/v1/assets?assetIds="
-user_info = "https://www.roblox.com/mobileapi/userinfo"
+user_info = "https://auth.roblox.com/v1/palisades-live/is-live"
 
 def getPublishUrl(assetTypeName, name, creatorId, isGroup):
     return (
         "https://www.roblox.com/ide/publish/uploadnewanimation?"
         f"assetTypeName={ assetTypeName }"
         f"&name={ name }"
-        "&description=kartfr🤑🤑"
+        "&description=kartFr🤑🤑"
         "&AllID=1"
         "&ispublic=False"
         "&allowComments=True"
